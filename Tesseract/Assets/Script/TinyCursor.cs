48d83662bd6e3a7f3cad129cb27a7e2d70f539d2
Vidalee <thomas.sirvent@free.fr>
2019-02-07 16:18:21 +0100

    styling

--- Tesseract/Assets/Script/TinyCursor.cs
+++ Tesseract/Assets/Script/TinyCursor.cs
@@ -15,21 +15,20 @@ public class TinyCursor : MonoBehaviour
     {
         GameObject g = GameObject.Find("Player");
         PlayerMovement pm = g.GetComponent<PlayerMovement>();
-        Vector3 center = pm.transform.position;
-        
+        Vector3 player = pm.transform.position;
+        Vector2 cursorPos = new Vector2();
         Vector2 cursorPosR = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(cursorPosR.y - center.y, cursorPosR.x - center.x);
-        float tinyY = Mathf.Sin(angle) * 1f;
-        float tinyX = Mathf.Cos(angle) * 1f;
-        Vector2 cursorPos = cursorPosR - new Vector2(tinyX, tinyY);
-        //Vector2 center = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
-        
-        if (pm == null) Debug.Log(":(");
-        else {
-            if (Vector2.Distance(cursorPosR, center) < 1f)
-                cursorPos = center;
-            transform.position = cursorPos;
+
+        if (Vector2.Distance(cursorPosR, player) < 1f)
+            cursorPos = player;
+        else
+        {
+            float angle = Mathf.Atan2(cursorPosR.y - player.y, cursorPosR.x - player.x);
+            float tinyY = Mathf.Sin(angle) * 1f;
+            float tinyX = Mathf.Cos(angle) * 1f;
+            cursorPos = cursorPosR - new Vector2(tinyX, tinyY);
         }
-        
+
+        transform.position = cursorPos;
     }
 }
