e1c4e45e78fedf77057f2154cd4bbae661fea5d3
Vidalee <thomas.sirvent@free.fr>
2019-02-07 15:28:35 +0100

    works flawlessly, maybe TuWG can get rid of the trigonometry?

--- Tesseract/Assets/Script/TinyCursor.cs
+++ Tesseract/Assets/Script/TinyCursor.cs
@@ -13,16 +13,23 @@ public class TinyCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(cursorPos.y, cursorPos.x);
+        GameObject g = GameObject.Find("Player");
+        PlayerMovement pm = g.GetComponent<PlayerMovement>();
+        Vector3 center = pm.transform.position;
+        
+        Vector2 cursorPosR = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(cursorPosR.y - center.y, cursorPosR.x - center.x);
         float tinyY = Mathf.Sin(angle) * 1f;
         float tinyX = Mathf.Cos(angle) * 1f;
-        cursorPos -= new Vector2(tinyX, tinyY);
-        Vector2 center = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
-        float radius = 1f;
-        //PLSFIX
-        if (Mathf.Abs(Input.mousePosition.x - center.x) < radius && Mathf.Abs(Input.mousePosition.y - center.y) < radius)
-            cursorPos = center;
-        transform.position = cursorPos;
+        Vector2 cursorPos = cursorPosR - new Vector2(tinyX, tinyY);
+        //Vector2 center = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+        
+        if (pm == null) Debug.Log(":(");
+        else {
+            if (Vector2.Distance(cursorPosR, center) < 1f)
+                cursorPos = center;
+            transform.position = cursorPos;
+        }
+        
     }
 }
