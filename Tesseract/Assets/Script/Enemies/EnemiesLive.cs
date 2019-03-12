c3e459d261600cc361a7eac1f335dc1e8bb72c82
William <william.grolleau@epita.fr>
2019-03-12 15:57:03 +0100

    Add player autoattack

---
+++ Tesseract/Assets/Script/Enemies/EnemiesLive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesLive : MonoBehaviour
+{
+    public int live;
+    
+    public void GetDamaged(int damage)
+    {
+        live -= damage;
+        if (live <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Tesseract/Assets/Script/Projectiles.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    private Vector3 direction;
+    private int speed;
+    private int damage;
+
+    public void Create(Vector3 direction, int speed, int damage)
+    {
+        this.direction = direction;
+        this.speed = speed;
+        this.damage = damage;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Enemies"))
+        {
+            Destroy(gameObject);
+            other.transform.GetComponent<EnemiesLive>().GetDamaged(damage);
+        }
+    }
+}
