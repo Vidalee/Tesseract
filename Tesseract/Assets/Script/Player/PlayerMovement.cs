ae6ca1b282e6eeccc36831b7244c743b770f33c1
William <william.grolleau@epita.fr>
2019-03-13 23:52:53 +0100

    Displacement animation

--- Tesseract/Assets/Script/Player/PlayerMovement.cs
+++ Tesseract/Assets/Script/Player/PlayerMovement.cs
@@ -9,25 +9,42 @@ public class PlayerMovement : MonoBehaviour
     public int Speed;
     public LayerMask blockingLayer;
     public Animator movementAnimator;
- 
+
     private void FixedUpdate()
     {
         PlayerDisplacement();
     }
 
-    private void PlayerAnimation(int x, int y)
+    private void PlayerMovingAnimation(int x, int y)
     {
         int speed = x != 0 ? x : y;
+        bool dir = x == 0;
         
-        movementAnimator.SetInteger("Speed", speed);
+        if (speed == 0)
+        {
+
+            movementAnimator.SetInteger("Speed", 0);
+            return;
+        }
+        
+        movementAnimator.SetInteger("Speed",speed);
+        movementAnimator.SetBool("Direction",dir);
+        
     }
+
+    private void PlayerProjectilesAnimation()
+    {
+        movementAnimator.SetBool("Projectiles", Input.GetMouseButtonDown(0));
+    }
+    
     private void PlayerDisplacement()
     {
         //Get and set 
         int xDir = (int) Input.GetAxisRaw("Horizontal");
         int yDir = (int) Input.GetAxisRaw("Vertical");
         
-        PlayerAnimation(xDir, yDir);
+        PlayerMovingAnimation(xDir, yDir);
+        PlayerProjectilesAnimation();
         
         Vector3 xVec = new Vector3(xDir,0,0);
         Vector3 yVec = new Vector3(0,yDir,0);
