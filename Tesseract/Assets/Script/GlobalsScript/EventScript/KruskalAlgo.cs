06b19d586bce078b9bcec9de1e0deecd5466f9a8
William <william.grolleau@epita.fr>
2019-03-22 02:16:58 +0100

    Understand principe of Union-Find function for subset in Kruskal algo, crate first struc for implementation, implement reccursion Find with path compression

---
+++ Tesseract/Assets/Script/GlobalsScript/EventScript/KruskalAlgo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class KruskalAlgo : MonoBehaviour
+ {
+     public Graph CreateGraph(int verticesCount, int edgesCount)
+     {
+         Graph graph = new Graph();
+         graph.VerticesCount = verticesCount;
+         graph.EdgeCounts = edgesCount;
+         graph.Edge = new Edge[edgesCount];
+
+         return graph;
+     }
+
+     public int Find(Subset[] subset, int i)
+     {
+         if (subset[i].Parent != i)
+             subset[i].Parent = Find(subset, subset[i].Parent);
+
+         return subset[i].Parent;
+     }
+ }
+ 
+ public struct Graph
+ {
+     public int EdgeCounts;
+     public int VerticesCount;
+     public Edge[] Edge;
+ 
+ }
+ 
+ public struct Edge
+ {
+     public int Sources;
+     public int Destination;
+     public int Weight;
+ }
+ 
+ public struct Subset
+ {
+     public int Parent;
+     public int rank;
+ }
+ 
