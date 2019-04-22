7c7f1c3885910ff2e4497ad69eb5049824e2d701
William <william.grolleau@epita.fr>
2019-04-22 18:14:43 +0200

    Fix archer, fix table

--- Tesseract/Assets/Script/FeetCollision.cs
+++ Tesseract/Assets/Script/FeetCollision.cs
@@ -26,7 +26,7 @@ public class FeetCollision : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Obstacle"))
+        if (other.CompareTag("Perspective"))
         {
             SpriteRenderer otherSpriteRenderer = other.transform.parent.GetComponent<SpriteRenderer>();
             spriteRenderer.sortingOrder = otherSpriteRenderer.sortingOrder - 1;
@@ -37,9 +37,9 @@ public class FeetCollision : MonoBehaviour
 
     private IEnumerator OnTriggerExit2D(Collider2D other)
     {
-        if(other.CompareTag("Obstacle"))
+        if(other.CompareTag("Perspective"))
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.5f);
             if((other.transform.position - transform.position).magnitude > 0.7f) spriteRenderer.sortingOrder = 100;
         }
     }
