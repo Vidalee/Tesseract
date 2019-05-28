ce501f3ed7d6d0f66d4e0b14fcd8d875a28d121f
DreamExe31 <adrien.anton-ludwig@epita.fr>
2019-05-28 15:38:10 +0200

    Adjustments in prefabs organisation

---
+++ Tesseract/Assets/Script/Enemies/GenerateEnemies.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Script.Enemies;
+using Script.Pathfinding;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public int MapHeight;
+    public int MapWidth;
+
+    public int seed;
+    
+    public static List<Transform> players = new List<Transform>();
+    public LayerMask BlockingLayer;
+
+    
+    [SerializeField] protected GameObject Enemy;
+    private EnemyData[,] _grid;
+    public static List<RoomData> RoomData;
+    public static bool[,] availablePosGrid;
+    
+    [SerializeField] protected List<EnemyData> Enemies;
+
+    private void Start()
+    {
+        //players.Add(PlayerManager.Player);
+        _grid = new EnemyData[MapHeight, MapWidth];
+        Random.InitState(seed); 
+        
+        foreach (RoomData roomData in RoomData)
+        {
+            //int roomSpace = roomData.Width * roomData.Height;
+            int enemiesNumber = 2;
+            while (enemiesNumber != 0)
+            {
+                int x = roomData.X1 + Random.Range(0, roomData.Width);
+                int y = roomData.Y1 + Random.Range(0, roomData.Height);
+                if (availablePosGrid[y + 1, x] && availablePosGrid[y, x] && _grid[y, x] == null)
+                {
+                    GameObject enemy = Instantiate(Enemy, new Vector3(x, y, 0), Quaternion.identity);
+                    EnemyData newEnemy = ScriptableObject.CreateInstance<EnemyData>();
+                    newEnemy.Create(Enemies[Random.Range(0, 2)], x, y);
+                    
+                    
+                    enemy.GetComponent<Attack>().Create(newEnemy, players[0]);
+                    enemy.GetComponent<EnemiesLive>().Create(newEnemy); 
+                    enemy.GetComponent<EnemiesMovement>().Create(newEnemy, players, BlockingLayer);
+                    enemy.GetComponent<Pathfinding>().Create(newEnemy);
+                    enemy.GetComponentInChildren<SpriteRenderer>().sprite = newEnemy.Sprite;
+
+                    enemiesNumber--;
+                }
+            }
+        }
+    }
+}
