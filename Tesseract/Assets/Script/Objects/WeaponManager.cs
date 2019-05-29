d8d907b81070d4c43e65f50641756942adbb5465
DreamExe31 <adrien.anton-ludwig@epita.fr>
2019-05-29 14:05:38 +0200

    Warrior weapon set up

--- Tesseract/Assets/Script/Objects/WeaponManager.cs
+++ Tesseract/Assets/Script/Objects/WeaponManager.cs
@@ -19,9 +19,9 @@ public class WeaponManager : MonoBehaviour
     }
 
     
-    public void PlaceWeapon(IEventArgs args)
+    public void AttackWithWeapon(IEventArgs args)
     {
-        /*
+        
         Vector3 move = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         bool dir =  Math.Abs(move.x) < Math.Abs(move.y);
         if (dir)
@@ -29,12 +29,10 @@ public class WeaponManager : MonoBehaviour
             if (move.y < 0)
             {
                 StartCoroutine(Attack(-0.052f, -0.002f, -0.122f, -0.009f));
-                Debug.Log("Down");
             }
             else
             {
                 StartCoroutine(Attack(0.049f, -0.007f, -0.218f, -0.014f));
-                Debug.Log("Up");
             }
         }
         else
@@ -42,17 +40,40 @@ public class WeaponManager : MonoBehaviour
             if (move.x < 0)
             {
                 StartCoroutine(Attack(0.049f, -0.007f, -0.218f, -0.014f));
-                Debug.Log("Left");
+                
             }
             else 
             {
                 StartCoroutine(Attack(0.049f, -0.007f, -0.218f, -0.014f));
-                Debug.Log("Right"); 
+                // Up -> x : 0.208 / y : 0.362
+                // Down -> x : 0.246 / y : -0.065
+                
+                // Center -> x : 0.112 / y : 0.148
+                
             }  
         }
-        */
     }
 
+
+    public void WeaponMovement(IEventArgs args)
+    {
+        EventArgsCoor move = args as EventArgsCoor;
+        if (move.X == 0 && move.Y == 0) return;
+        bool dir =  Math.Abs(move.X) < Math.Abs(move.Y);
+        if (dir && move.Y > 0)
+        {
+            _sprite.enabled = true;
+        }
+        else _sprite.enabled = false;
+    }
+
+
+    public void PlaceWeapon(float x, float y, float angle)
+    {
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.localPosition = new Vector3(x, y);
+    }
+    
     IEnumerator Attack(float a, float b, float c, float d)
     {
         _sprite.enabled = true;
