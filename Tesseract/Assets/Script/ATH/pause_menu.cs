278db833720bee43133bf658a043e587a2339d26
William <william.grolleau@epita.fr>
2019-05-30 23:14:39 +0200

    Fix pause

--- Tesseract/Assets/Script/ATH/pause_menu.cs
+++ Tesseract/Assets/Script/ATH/pause_menu.cs
@@ -1,9 +1,38 @@
-﻿using UnityEngine;
+﻿using Script.GlobalsScript;
+using UnityEngine;
 
 public class pause_menu : MonoBehaviour
 {
     public GameObject Canvas;
+    public bool state;
+    public int waitTime;
+    public bool wait;
 
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && !wait)
+        {
+            state = !state;
+            wait = true;
+            
+            if(state) Active();
+            if(!state) Desactive();
+        }
+
+        if (wait)
+        {
+            if (waitTime > 50)
+            {
+                wait = false;
+                waitTime = 0;
+            }
+            else
+            {
+                waitTime++;
+            }
+        }
+    }
+    
     void Start()
     {
         Canvas.SetActive(false);
@@ -11,13 +40,14 @@ public class pause_menu : MonoBehaviour
 
     public void Active()
     {
+        StaticData.pause = true;
         Canvas.SetActive(true);
         Time.timeScale = 0;
-        
     }
 
     public void Desactive()
     {
+        StaticData.pause = false;
         Canvas.SetActive(false);
         Time.timeScale = 1;
     }
--- Tesseract/Assets/Script/GlobalsScript/StaticData.cs
+++ Tesseract/Assets/Script/GlobalsScript/StaticData.cs
@@ -19,5 +19,7 @@ namespace Script.GlobalsScript
         }
         
         public static int Seed;
+
+        public static bool pause;
     }
 }
