450d2d5f32355f2ee93dea84dac48bba6d1a9f53
DreamExe31 <adrien.anton-ludwig@epita.fr>
2019-06-02 02:55:52 +0200

    Boss

--- Tesseract/Assets/Script/Boss/PlayerSpawnBoss.cs
+++ Tesseract/Assets/Script/Boss/PlayerSpawnBoss.cs
@@ -1,12 +1,19 @@
-﻿using Script.GlobalsScript;
+﻿using System.Collections;
+using Script.GlobalsScript;
 using UnityEngine;
 
 public class PlayerSpawnBoss : MonoBehaviour
 {
     public Transform PlayerManager;
+    public GameObject boss;
 
     private void Start()
     {
-        Instantiate(PlayerManager, new Vector3(5, 5, 0), Quaternion.identity).GetComponent<PlayerManager>().Create(10, 2, int.Parse((string) Coffre.Regarder("id")), (string) Coffre.Regarder("mode") == "solo");
+        Coffre.Créer();
+        Transform o = Instantiate(PlayerManager, new Vector3(5, 5, 0), Quaternion.identity);
+        o.GetComponent<PlayerManager>().Create(10, 2, int.Parse((string) Coffre.Regarder("id")), (string) Coffre.Regarder("mode") == "solo");
+        //o.GetComponent<PlayerManager>().Create(10, 2, 0, true);
+        boss.GetComponent<BossAttack>().Create(o.GetChild(0));
+        boss.GetComponent<BossMovement>().Create(o.GetChild(0));
     }
 }
